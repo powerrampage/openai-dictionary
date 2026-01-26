5204ed00b599c9e8495e6d1002d12ff42bde4fa3
Mironshokh <64687619+powerrampage@users.noreply.github.com>
2026-01-26 17:10:26 +0500

    accept app parameter

--- src/dictionary/index.ts
+++ src/dictionary/index.ts
@@ -3,25 +3,19 @@ import { DictionaryService } from "./service";
 
 const dictionaryService = new DictionaryService();
 
-export function dictionaryRoutes() {
-	return new Elysia({ prefix: "/dictionary" })
-		.get("/stream", async function*({ set, query }) {
+export function dictionaryRoutes(app: Elysia) {
+	return app.get("/dictionary/stream", async function*({ set, query }) {
 			set.headers = {
 				"Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "X-Content-Type-Options": "nosniff"
+				"Cache-Control": "no-cache",
+				"Connection": "keep-alive",
+				"X-Content-Type-Options": "nosniff"
 			}
-
-			for await (const chunk of dictionaryService.streamDefinition(query.word)) {
-				yield chunk;
-			}
+	
+			for await (const chunk of dictionaryService.streamDefinition(query.word)) yield chunk;
 		}, {
 			query: t.Object({
-				word: t.String({
-					minLength: 3,
-					maxLength: 50
-				})
+				word: t.String({ minLength: 3, maxLength: 50 })
 			})
 		})
 }
