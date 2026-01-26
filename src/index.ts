8cf51c6c69a2cf6381b50539175d52670e5494d4
Mironshokh <64687619+powerrampage@users.noreply.github.com>
2026-01-26 17:11:49 +0500

    refactor elysia app

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { Elysia } from "elysia";
 import { dictionaryRoutes } from "./dictionary";
 
 const app = new Elysia()
-	.get("/health", () => ({ status: "ok" }))
-	.use(dictionaryRoutes())
-	.listen(3000);
+	.get("/health", () => ({ status: "ok" }));
+
+// dictionaryRoutes(app);
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
